54adfde93834a3053be92fcee7178be826b4b157
Florian Völker <Neirolf2030@web.de>
2019-12-03 14:46:46 +0100

    fixed missing return in iterator helper

--- src/iterator-helper.ts
+++ src/iterator-helper.ts
@@ -1,10 +1,20 @@
 export interface IteratorAndIterable<T> extends Iterator<T, T>, Iterable<T> { }
 
-export function produceIteratorAndIterable<T>(fnc: () => { done: boolean, value: undefined | T }): IteratorAndIterable<T> {
+export function produceIteratorAndIterable<T>(fnc: Iterator<T>['next']): IteratorAndIterable<T> {
   const ite = {
-    next: () => fnc,
+    next: () => fnc(),
     [Symbol.iterator]: () => ite
   };
 
-  return
+  return ite;
+}
+
+export function extendIterable<V, U>(iterable: Iterator<V>, mapper: (val: V) => U): IteratorAndIterable<U> {
+  return produceIteratorAndIterable(() => {
+    const n = iterable.next();
+    const done = n.done;
+    const value = n.value;
+
+    return { done, value: done ? undefined : mapper(value) };
+  });
 }
